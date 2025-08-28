74cef85b4e49a3e54222cd847c699da97b9c7854
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2025-08-28 18:13:27 +0530

    Updated code

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-
 import App from './App.jsx';
 import './assets/styles/global.css';
 
